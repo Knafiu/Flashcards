f266ebedc7d3b55c01db0abf56ad89933bcc05a4
Khaleel Nafiu <knafiu2@bu.edu>
2026-03-09 16:23:17 -0400

    Completed NBA flashcards project

---
+++ Flashcards/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const cards = [["Start", "Press to start game :)"],
+  ["Which player has scored the most points in NBA history?","LeBron James"],
+    ["Which team has won the most NBA championships?","Boston Celtics"],
+    ["Who is the only player to win MVP unanimously?","Stephen Curry"],
+    ["Which player scored 100 points in a single NBA game?","Wilt Chamberlain"],
+    ["What team drafted Michael Jordan in 1984?","Chicago Bulls"],
+    ["Which NBA player is known as “The Greek Freak”? ","Giannis Antetokounmpo"],
+    ["Which team completed the only 73-win regular season in NBA history?","Golden State Warriors (2015–16)"],
+    ["Who holds the record for the most career assists in the NBA?","Who holds the record for the most career assists in the NBA?"],
+    ["Which player won six NBA championships with the Chicago Bulls in the 1990s?", "Michael Jordan"],
+    ["Which player is known as “King James”?", "LeBron James"]
+  ];
+  const [pos1, setPos1] = useState(0);
+  const [pos2, setPos2] = useState(0);
+  const [display, setDisplay] = useState(cards[0][0]);
+
+
+
+  function Show(){
+    if(pos2 === 0){
+      setPos2(1);
+      setDisplay(cards[pos1][1]);
+    }
+    else{
+      setPos2(0);
+      setDisplay(cards[pos1][0]);
+    }
+  }
+  function handleClickForward(){
+    if (pos1 < cards.length - 1){
+      const nextPos = pos1 + 1;
+      setPos1(nextPos);
+      setDisplay(cards[nextPos][0]);
+    }
+  }
+  function handleClickBackwards(){
+    if (pos1 > 0){
+      const nextPos = pos1 - 1;
+      setPos1(nextPos);
+      setDisplay(cards[nextPos][0]);
+    }
+  }
+  
+  return (
+    <>
+    <div className='flashcards'>
+    <div className='header'>
+      <h1>NBA Trivia</h1>
+      <h3>Let's test your basketball knowledge</h3>
+      <p>Number of Cards: 10</p>
+    </div>
+    <div className='content'>
+      <div className='display' onClick={Show}>
+        {display}
+      </div>
+      <div>
+      <button onClick={handleClickBackwards}>←</button>
+      <button onClick={handleClickForward}>→</button>
+      </div>
+      
+    </div>
+    </div>
+   
+     
+    </>
+  )
+}
+
+export default App
